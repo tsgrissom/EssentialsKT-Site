ceefa09609cedceb1f82a7fa7734e1b125c8ce3d
Tyler Grissom <tylergrissom@me.com>
2023-10-26 16:10:33 -0500

    Improved links with aria labels, added navigation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('node:path');
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 app.get('/', (req, res) => {
